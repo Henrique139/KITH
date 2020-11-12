19fbedf8f9389e386fb593398599855041ff1d7f
Henrique139 <70975650+Henrique139@users.noreply.github.com>
2020-11-12 14:42:57 -0300

    Update instale.py

--- instale.py
+++ instale.py
@@ -4,7 +4,7 @@ subprocess.call(["apt install git", "-la"], shell=True)
 
 subprocess.call(["apt update", "-la"], shell=True)
 
-subprocess.call(["apt upgrate", "-la"], shell=True)
+subprocess.call(["apt upgrade", "-la"], shell=True)
 
 subprocess.call(["apt install nano", "-la"], shell=True)
 
@@ -21,3 +21,5 @@ subprocess.call(["pkg install unstable-repo", "-la"], shell=True)
 subprocess.call(["pkg install coreutils", "-la"], shell=True)
 
 subprocess.call(["apt install pearl", "-la"], shell=True)
+
+subprocess.call(["apt install curl", "-la"], shell=True)

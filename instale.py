60dd8a02488fe89eacb1a26e85070ed64cdc0e9d
Henrique César Bispo Santos <cesarhenriqueabc@gmail.com>
2021-06-17 20:12:54 -0300

    Deixando o 'instale.py' mais amigável

--- instale.py
+++ instale.py
@@ -1,27 +1,55 @@
 import subprocess
+import os
+from time import sleep 
 
-subprocess.call(["apt install git", "-la"], shell=True)
+def inst():
+    os.system('clear')
+    sleep(2)
+
+
+os.system('clear')
+
+print('=== Instalando algumas coisas que podem ser úteis ===')
+
+inst()
+
+print('=== Atualizando Lista de pacotes ===')
 
 subprocess.call(["apt update", "-la"], shell=True)
 
 subprocess.call(["apt upgrade", "-la"], shell=True)
 
-subprocess.call(["apt-get update", "-la"], shell=True)
+subprocess.call(["pkg update", "-la"], shell=True)
 
-subprocess.call(["apt-get upgrade", "-la"], shell=True)
+subprocess.call(["pkg upgrade", "-la"], shell=True)
 
+inst()
+
+print('=== Instalando editor de texto  NANO ===')
 subprocess.call(["apt install nano", "-la"], shell=True)
 
+inst()
+
+print('=== instalando wget ===')
 subprocess.call(["apt install wget", "-la"], shell=True)
 
-subprocess.call(["termux-tools", "-la"], shell=True)
+inst()
 
+print('=== Instalando PHP ===')
 subprocess.call(["apt install php", "-la"], shell=True)
 
-subprocess.call(["pkg install unstable-repo", "-la"], shell=True)
+inst()
 
+print('=== Instalando coreutils ===')
 subprocess.call(["pkg install coreutils", "-la"], shell=True)
 
+inst()
+
+print('===  Instalando Curl ===')
 subprocess.call(["apt install curl", "-la"], shell=True)
 
+inst()
+
+print('===  Iniciando o KITH ===')
+sleep(3)
 subprocess.call(["python kith.py", "-la"],shell=True)
